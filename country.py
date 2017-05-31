d28935b3e311a2e2c7631ee2cfb6a60304f32ce1
Mathieu Alorent <github@kumy.net>
2017-05-31 23:22:03 +0200

    Retry connection

--- country.py
+++ country.py
@@ -7,21 +7,24 @@ except:
   print("Failed to load config, declare your conf in config.py. see config.py.ex")
   sys.exit(1)
 
-import sqlalchemy
+from sqlalchemy import create_engine
+
+engine = create_engine(config.dsn, pool_recycle=300)
+connection = engine.connect()
 
 class Country:
 
-  connection = None
-
-  def __init__(self):
-    engine = sqlalchemy.create_engine(config.dsn, pool_recycle=300)
-    self.connection = engine.connect()
-  
   def get_country(self, lat, lon):
-    result = self.connection.execute("""SELECT iso_2 FROM boundaries
-LEFT JOIN codes ON boundaries.iso1 = codes.ISO_3
-WHERE ST_Contains(boundaries.shape, GeomFromText('POINT(%f %f)'))""" % (lon, lat)
-)
+    try:
+      connection.execute("SELECT 1")
+    except:
+      pass
+
+    result = connection.execute("""
+      SELECT iso_2 FROM boundaries
+      LEFT JOIN codes ON boundaries.iso1 = codes.ISO_3
+      WHERE ST_Contains(boundaries.shape, GeomFromText('POINT(%f %f)'))""" % (lon, lat)
+      )
 
     for row in result:
       return row['iso_2']
